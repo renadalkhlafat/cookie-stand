c782724935bee147b609b3acd9a5627d5cb93c05
renadalkhlafat <renadsalem8888@gmail.com>
2021-06-14 01:42:58 +0300

    demo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 'use strict';
+let mainElement= document.getElementById('dailySales');
+
 // --------------------Seattle object ---------------
-let Seattle ={
+let Seattle = 
+{
     name:'Seattle',
     minCustomers:23,
     maxCustomers:65,
@@ -17,8 +20,41 @@ let Seattle ={
       let amount=Math.ceil(this.averageCookies*this.customersPerHour(1,14));
       return amount;
     },
-
+    // render: function(){
+    //     let locName=document.createElement('h2');//locName
+    //     locName.textContent=this.name;
+    //     mainElement.appendChild(locName);
+    //     let unorderedList=document.createElement('ul');
+    //     mainElement.appendChild(unorderedList);
+    //     let totalCookies=0;
+    //     for(let a=6; a<21; a++ ){
+    //     //   let randmCustomer=Math.floor(Math.random() * this.maximumCustemers) + this.minimumCustomers;
+    //     //   let randmCustomerPurshase=randmCustomer*this.avgCookiePerCustomer;
+    //     //   randmCustomerPurshase.toFixed(0);
+    //       if(a<12){
+    //         let hour=document.createElement('li');
+    //         unorderedList.appendChild(hour);
+    //         hour.textContent=`${a}am:${this.customersPerHour()} cookies`;
+    //         totalCookies=totalCookies+this.customersPerHour();
+    //       }else if(a===12){
+    //         let hour=document.createElement('li');
+    //         unorderedList.appendChild(hour);
+    //         hour.textContent=`${a}pm:${this.customersPerHour()} cookies`;
+    //       }else if(a>12 && a<20){
+    //         let hour=document.createElement('li');
+    //         unorderedList.appendChild(hour);
+    //         hour.textContent=`${a-12}pm:${this.customersPerHour()} cookies`;
+    //       }else if(a===20){
+    //         let totalSales=document.createElement('li');
+    //         unorderedList.appendChild(totalSales);
+    //         totalSales.textContent=`Total:${totalCookies} cookies`;
+    //       }
+    //     }
+    // }
 }
+console.log( Seattle.cookiesPurchased());
+// Seattle.render();
+console.log(Seattle.name)
 
 // --------------------Tokyo object ---------------
 let Tokyo ={
@@ -95,5 +131,5 @@ let Lima ={
       }
 }
 
-let location = [Seattle, Tokyo, Dubai, Paris, Lima];
 
+let shopName = [Seattle, Tokyo, Dubai, Paris, Lima];
